9c19c73c7920ff28a1369adbd9402e07da77e362
AlperSnr <alpercemalsener3352@gmail.com>
2024-11-12 14:30:55 +0300

    Hafta 2 eklendi

---
+++ Kodlar/2. Hafta/AwakeStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeStart : MonoBehaviour
+{
+    public int sayi = 15;
+
+    private int sayi2;
+
+    public float ondalikli = 0.5f;
+
+    public bool doğru = false;
+
+    public string yazi = "Alper";
+
+
+    private void Awake()
+    {
+        print("awake");
+    }
+    void Start()
+    {
+        print("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("update");
+    }
+}
---
+++ Kodlar/2. Hafta/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject küp;
+    public Vector3 vektör;
+
+    public float speed;
+
+    private int sayi;
+    void Start()
+    {
+        küp.transform.position += vektör;
+
+        vektör.z = 5 * 10;
+
+        vektör = new Vector3(5,5,5);
+    }
+
+    void Update()
+    {
+        //Input.GetKey() verilen key için o an basılı olup olmadığının bilgisini verir.
+        //Time.deltaTime = 1/FPS değerini verir.
+        if (Input.GetKey(KeyCode.W))
+        {
+            küp.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            küp.transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            küp.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            küp.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
